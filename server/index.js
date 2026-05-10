edec50b73c2d11a5b2ffd22f51276c4780b225f6
yeasaleh2002 <yeasaleh2002@gmail.com>
2026-05-10 11:26:12 +0600

    upload vercel

--- server/index.js
+++ server/index.js
@@ -66,3 +66,5 @@ app.use(require('./middleware/errorMiddleware'));
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, console.log(`Server running on port ${PORT}`));
+
+module.exports = app;
